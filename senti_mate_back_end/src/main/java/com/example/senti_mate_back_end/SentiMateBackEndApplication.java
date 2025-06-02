7b00949c238f560c925055d902a8f9ff972a32f9
va1k1r1 <jinyung13@gmail.com>
2025-06-02 10:57:14 +0900

    Initialize project with backend (Spring Boot) and frontend (React + Vite) setup.

---
+++ senti_mate_back_end/src/main/java/com/example/senti_mate_back_end/SentiMateBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.senti_mate_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentiMateBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentiMateBackEndApplication.class, args);
+    }
+
+}
---
+++ senti_mate_back_end/src/test/java/com/example/senti_mate_back_end/SentiMateBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.senti_mate_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentiMateBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
